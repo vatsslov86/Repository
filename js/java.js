e980cac95e2282f0393b4c5d744ca8e070b6180f
vatsslov86 <vatsslov86@mail.ru>
2016-02-16 16:12:29 +0300

    Новые файлы
    Добавлены новые файлы

---
+++ js/java.js
@@ -0,0 +1,23 @@
+﻿// JavaScript Document
+$(document).ready(function(){
+	$('#show-map').click(function(){
+		$('#map').slideToggle(1000);
+		});
+	});
+
+//Плавное перемещение
+/*
+$(function(){
+$('.scr').bind('click',function(){
+var speed = 1000; // Скорость движения
+if($(this).attr('href')=='#down'){
+$('html').animate({scrollTop:$(document).height()}, speed,'swing');
+} else if($(this).attr('href')=='#top'){
+$('html').animate({scrollTop:0}, speed,'swing');
+} else {
+$('html').animate({scrollTop: $(this.hash).offset().top}, speed,'swing');
+}
+return false;
+});
+
+*/
